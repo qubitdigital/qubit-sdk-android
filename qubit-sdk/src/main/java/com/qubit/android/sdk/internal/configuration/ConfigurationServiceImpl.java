8e59d46f3a56e7d9b0dff3a821f37e9e13ddab10
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2017-08-28 15:29:56 +0200

    Missing methods in configuration service.

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/configuration/ConfigurationServiceImpl.java
+++ qubit-sdk/src/main/java/com/qubit/android/sdk/internal/configuration/ConfigurationServiceImpl.java
@@ -1,4 +1,20 @@
 package com.qubit.android.sdk.internal.configuration;
 
 public class ConfigurationServiceImpl implements ConfigurationService {
+
+  @Override
+  public void registerInitializationListener(InitializationListener listener) {
+
+  }
+
+  @Override
+  public boolean isInitialized() {
+    return false;
+  }
+
+  @Override
+  public Configuration getConfiguration() {
+    return null;
+  }
+  
 }
