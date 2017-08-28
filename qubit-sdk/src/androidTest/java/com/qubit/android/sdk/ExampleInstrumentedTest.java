40795b673457df0eba02ef0c84e454062d1c4c6c
Agnieszka Adamczyk <agnieszka.adamczyk@miquido.com>
2017-08-28 11:04:02 +0200

    Added checkstyle and fixed violated rules

--- qubit-sdk/src/androidTest/java/com/qubit/android/sdk/ExampleInstrumentedTest.java
+++ qubit-sdk/src/androidTest/java/com/qubit/android/sdk/ExampleInstrumentedTest.java
@@ -3,7 +3,6 @@ package com.qubit.android.sdk;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +15,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.qubit.android.sdk.test", appContext.getPackageName());
-    }
+    assertEquals("com.qubit.android.sdk.test", appContext.getPackageName());
+  }
 }
