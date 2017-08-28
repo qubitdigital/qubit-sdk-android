b3514c7ffd92c8f6de7e4abab6c16d7f88f3d226
Agnieszka Adamczyk <agnieszka.adamczyk@miquido.com>
2017-08-28 16:32:28 +0200

    Configuration's fields

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/configuration/Configuration.java
+++ qubit-sdk/src/main/java/com/qubit/android/sdk/internal/configuration/Configuration.java
@@ -1,6 +1,17 @@
 package com.qubit.android.sdk.internal.configuration;
 
 public class Configuration {
-  // TODO
+
+  private String endpoint; // TODO default "gong-eb.qubit.com"
+  private String dataLocation; // TODO default "EU" ?
+  private int configurationReloadInterval; // TODO default 60
+  private int queueTimeout; // TODO default 60
+  private String vertical; // TODO default "ec" ?
+  private String namespace; // TODO default ""
+  private long propertyId; // TODO default 1234 ?
+  private boolean disabled; // TODO default false ?
+  private String lookupAttributeUrl; // TODO default "https://lookup.qubit.com" ?
+  private int lookupGetRequestTimeout; // TODO default 5 ?
+  private int lookupCacheExpireTime; // TODO default 60 ?
 
 }
