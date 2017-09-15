b90b1a78a46eb8b8486f7298ad18f763dee07afc
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2017-09-15 14:45:48 +0200

    [feature/QB-31/configurable_configuration_url] Configuration url configurable via changing properties in internal classes

--- test-app/src/main/java/com/qubit/android/sdk/testapp/TestApplication.java
+++ test-app/src/main/java/com/qubit/android/sdk/testapp/TestApplication.java
@@ -5,6 +5,7 @@ import android.os.StrictMode;
 import android.util.Log;
 import com.qubit.android.sdk.api.QubitSDK;
 import com.qubit.android.sdk.api.logging.QBLogLevel;
+import com.qubit.android.sdk.internal.configuration.ConfigurationServiceImpl;
 
 public class TestApplication extends Application {
 
@@ -21,6 +22,10 @@ public class TestApplication extends Application {
         .detectNetwork()   // or .detectAll() for all detectable problems
         .penaltyLog()
         .build());
+
+    // Only for debugging purposes. It is not officially public API. DO NOT USE IT.
+    //ConfigurationServiceImpl.configurationUrl = "http://s3-eu-west-1.amazonaws.com/ljazgar-qubit/config/";
+    //ConfigurationServiceImpl.enforceDownloadOnStart = false;
 
     QubitSDK.initialization()
         .inAppContext(this)
