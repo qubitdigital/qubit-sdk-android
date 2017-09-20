3a8f4c4679622146190fe3a16e23a7c82f61d8a8
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2017-09-20 18:22:14 +0200

    [feature/QB-77/lookup_persistency] Lookup persistency. Refactoring of Lookup startup: First lookup request, than load from local storage.

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/lookup/repository/LookupRepositoryImpl.java
+++ qubit-sdk/src/main/java/com/qubit/android/sdk/internal/lookup/repository/LookupRepositoryImpl.java
@@ -1,15 +1,50 @@
 package com.qubit.android.sdk.internal.lookup.repository;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.qubit.android.sdk.internal.logging.QBLogger;
+
 public class LookupRepositoryImpl implements LookupRepository {
+
+  private static final QBLogger LOGGER = QBLogger.getFor("LookupRepository");
+
+  private static final String PREFERENCES_FILE = "qubit_lookup";
+  private static final String LOOKUP_KEY = "lookup";
+
+  private final Context appContext;
+  private Gson gson;
+
+  public LookupRepositoryImpl(Context appContext) {
+    this.appContext = appContext;
+  }
 
   @Override
   public void save(LookupCache lookupCache) {
-    // TODO
+    SharedPreferences sharedPreferences = appContext.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+    sharedPreferences.edit()
+        .putString(LOOKUP_KEY, getGson().toJson(lookupCache))
+        .commit();
   }
 
   @Override
   public LookupCache load() {
-    // TODO
-    return null;
+    try {
+      SharedPreferences sharedPref = appContext.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+      String lookupCacheJson = sharedPref.getString(LOOKUP_KEY, null);
+      return lookupCacheJson != null ? getGson().fromJson(lookupCacheJson, LookupCache.class) : null;
+    } catch (JsonSyntaxException e) {
+      LOGGER.e("Error parsing lookup data JSON from local storage.", e);
+      return null;
+    }
   }
+
+  private Gson getGson() {
+    if (gson == null) {
+      gson = new Gson();
+    }
+    return gson;
+  }
+
 }
