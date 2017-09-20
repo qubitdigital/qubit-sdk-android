5425a0b3c77c1b307179b0b0adcf4cc2d4228f8d
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2017-09-20 12:29:59 +0200

    [master] Remove unused class SessionRepositoryProvider.

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/session/repository/SessionRepositoryProvider.java
+++
@@ -1,7 +0,0 @@
-package com.qubit.android.sdk.internal.session.repository;
-
-import android.os.Handler;
-
-public interface SessionRepositoryProvider {
-  SessionRepository provide(Handler handler);
-}
