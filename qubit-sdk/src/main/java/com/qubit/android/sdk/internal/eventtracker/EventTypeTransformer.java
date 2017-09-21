b2f0d1a6d8322b11debf083bf45ae234f4302d36
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2017-09-21 07:40:27 +0200

    [master] Removing handling vertical parameter.

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/eventtracker/EventTypeTransformer.java
+++ qubit-sdk/src/main/java/com/qubit/android/sdk/internal/eventtracker/EventTypeTransformer.java
@@ -7,36 +7,23 @@ public class EventTypeTransformer {
   private static final String DOT = ".";
 
   private final String namespace;
-  private final String vertical;
 
-  public EventTypeTransformer(String namespace, String vertical) {
+  public EventTypeTransformer(String namespace) {
     this.namespace = namespace;
-    this.vertical = vertical;
   }
 
   public EventTypeTransformer(Configuration configuration) {
     this.namespace = configuration.getNamespace();
-    this.vertical = configuration.getVertical();
   }
 
   public String transform(String sourceEventType) {
-    return transformEventType(sourceEventType, namespace, vertical);
+    return transformEventType(sourceEventType, namespace);
   }
 
-  private static String transformEventType(String sourceEventType, String namespace, String vertical) {
+  private static String transformEventType(String sourceEventType, String namespace) {
     return sourceEventType.startsWith("qubit.")
         ? sourceEventType
-        : addNamespace(addVertical(sourceEventType, vertical), namespace);
-  }
-
-  private static String addVertical(String sourceEventType, String vertical) {
-    if (vertical == null || vertical.isEmpty()
-        || sourceEventType.contains(DOT)
-        || sourceEventType.startsWith(vertical)) {
-      return sourceEventType;
-    }
-    String capitalizedSourceType = sourceEventType.substring(0, 1).toUpperCase() + sourceEventType.substring(1);
-    return vertical + capitalizedSourceType;
+        : addNamespace(sourceEventType, namespace);
   }
 
   private static String addNamespace(String sourceEventType, String namespace) {
--- qubit-sdk/src/test/java/com/qubit/android/sdk/internal/eventtracker/EventTypeTransformerTest.java
+++ qubit-sdk/src/test/java/com/qubit/android/sdk/internal/eventtracker/EventTypeTransformerTest.java
@@ -8,26 +8,19 @@ public class EventTypeTransformerTest {
   
   @Test
   public void transform() throws Exception {
-    assertEquals("ecView", transform("ecView", null, null));
-    assertEquals("ecView", transform("ecView", "", ""));
+    assertEquals("ecView", transform("ecView", null));
+    assertEquals("ecView", transform("ecView", ""));
 
-    assertEquals("ecView", transform("ecView", "", "ec"));
-    assertEquals("n.ecView", transform("n.ecView", "", "ec"));
-    assertEquals("ecView", transform("View", "", "ec"));
-    assertEquals("ecView", transform("view", "", "ec"));
-    assertEquals("ns.view", transform("ns.view", "", "ec"));
-    assertEquals("ecV", transform("v", "", "ec"));
+    assertEquals("n.ecView", transform("n.ecView", ""));
+    assertEquals("ns.view", transform("ns.view", ""));
 
-    assertEquals("ns1.ecView", transform("ns1.ecView", "ns", null));
-    assertEquals("qubit.ecView", transform("qubit.ecView", "ns", null));
-    assertEquals("ns.ecView", transform("ecView", "ns", null));
-
-    assertEquals("ns.ecView", transform("view", "ns", "ec"));
-
+    assertEquals("ns1.ecView", transform("ns1.ecView", "ns"));
+    assertEquals("qubit.ecView", transform("qubit.ecView", "ns"));
+    assertEquals("ns.ecView", transform("ecView", "ns"));
   }
   
-  private String transform(String sourceType, String namespace, String vertical) {
-    return new EventTypeTransformer(namespace, vertical).transform(sourceType);
+  private String transform(String sourceType, String namespace) {
+    return new EventTypeTransformer(namespace).transform(sourceType);
   }
 
 }
