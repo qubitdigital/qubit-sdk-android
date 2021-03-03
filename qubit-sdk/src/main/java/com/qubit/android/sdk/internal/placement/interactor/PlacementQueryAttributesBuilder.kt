d01547248214ae35d12b64c4c988a22748c7b801
Jan Hudzik <jan.hudzik@gmail.com>
2021-03-03 22:50:38 +0100

    Refactored attributes building logic by creating default attributes schemas

--- qubit-sdk/src/main/java/com/qubit/android/sdk/internal/placement/interactor/PlacementQueryAttributesBuilder.kt
+++ qubit-sdk/src/main/java/com/qubit/android/sdk/internal/placement/interactor/PlacementQueryAttributesBuilder.kt
@@ -9,7 +9,17 @@ internal class PlacementQueryAttributesBuilder {
   companion object {
     internal const val VISITOR_ATTRIBUTE_KEY = "visitor"
     internal const val USER_ATTRIBUTE_KEY = "user"
+    internal val USER_ATTRIBUTE_SCHEMA: List<AttributeProperty> = listOf(
+        AttributeProperty.StringType("id"),
+        AttributeProperty.StringType("email")
+    )
     internal const val VIEW_ATTRIBUTE_KEY = "view"
+    internal val VIEW_ATTRIBUTE_SCHEMA: List<AttributeProperty> = listOf(
+        AttributeProperty.StringType("currency"),
+        AttributeProperty.StringType("type"),
+        AttributeProperty.ArrayType("subtypes"),
+        AttributeProperty.StringType("language")
+    )
   }
 
   internal fun buildJson(
@@ -18,10 +28,8 @@ internal class PlacementQueryAttributesBuilder {
       cachedAttributes: Map<String, JsonObject>
   ) = JsonObject().apply {
     add(VISITOR_ATTRIBUTE_KEY, buildVisitorAttributesJson(deviceId))
-    add(USER_ATTRIBUTE_KEY, customAttributes, cachedAttributes,
-        { convertToUserAttribute(it) }, { buildEmptyUserAttribute() })
-    add(VIEW_ATTRIBUTE_KEY, customAttributes, cachedAttributes,
-        { convertToViewAttribute(it) }, { buildEmptyViewAttribute() })
+    add(USER_ATTRIBUTE_KEY, USER_ATTRIBUTE_SCHEMA, customAttributes, cachedAttributes)
+    add(VIEW_ATTRIBUTE_KEY, VIEW_ATTRIBUTE_SCHEMA, customAttributes, cachedAttributes)
 
     customAttributes?.keySet()
         ?.filter { !has(it) }
@@ -30,18 +38,17 @@ internal class PlacementQueryAttributesBuilder {
 
   private fun JsonObject.add(
       key: String,
+      schema: List<AttributeProperty>,
       customAttributes: JsonObject?,
-      cachedAttributes: Map<String, JsonObject>,
-      converter: (JsonObject) -> JsonObject,
-      emptyBuilder: () -> JsonObject
+      cachedAttributes: Map<String, JsonObject>
   ) {
     val value = (customAttributes?.get(key) as? JsonObject)
         // custom attributes passed by SDK user - should contain all the fields from schema and nothing more
-        ?.let { converter(it) }
+        ?.let { convertToAttribute(schema, it) }
     // cached attributes - skipped if set by SDK user
         ?: cachedAttributes[key]
         // if expected attribute is missing, then we should add empty ones
-        ?: emptyBuilder()
+        ?: buildEmptyAttribute(schema)
     add(key, value)
   }
 
@@ -50,28 +57,27 @@ internal class PlacementQueryAttributesBuilder {
 //    addProperty("userAgentString", "")  // TODO set value
   }
 
-  private fun convertToUserAttribute(sourceJsonObject: JsonObject) = JsonObject().apply {
-    addStringValueOrDefault(sourceJsonObject, "id")
-    addStringValueOrDefault(sourceJsonObject, "email")
+  private fun convertToAttribute(
+      schema: List<AttributeProperty>,
+      sourceJsonObject: JsonObject
+  ) = JsonObject().apply {
+    schema.forEach {
+      when (it) {
+        is AttributeProperty.StringType -> addStringValueOrDefault(sourceJsonObject, it.name)
+        is AttributeProperty.ArrayType -> addArrayValueOrDefault(sourceJsonObject, it.name)
+      }
+    }
   }
 
-  private fun buildEmptyUserAttribute() = JsonObject().apply {
-    addProperty("id", "")
-    addProperty("email", "")
-  }
-
-  private fun convertToViewAttribute(sourceJsonObject: JsonObject) = JsonObject().apply {
-    addStringValueOrDefault(sourceJsonObject, "currency")
-    addStringValueOrDefault(sourceJsonObject, "type")
-    addArrayValueOrDefault(sourceJsonObject, "subtypes")
-    addStringValueOrDefault(sourceJsonObject, "language")
-  }
-
-  private fun buildEmptyViewAttribute() = JsonObject().apply {
-    addProperty("currency", "")
-    addProperty("type", "")
-    add("subtypes", JsonArray())
-    addProperty("language", "")
+  private fun buildEmptyAttribute(
+      schema: List<AttributeProperty>
+  ) = JsonObject().apply {
+    schema.forEach {
+      when (it) {
+        is AttributeProperty.StringType -> addProperty(it.name, "")
+        is AttributeProperty.ArrayType -> add(it.name, JsonArray())
+      }
+    }
   }
 
   private fun JsonObject.addStringValueOrDefault(sourceJsonObject: JsonObject, propertyName: String) {
@@ -84,5 +90,14 @@ internal class PlacementQueryAttributesBuilder {
   private fun JsonObject.addArrayValueOrDefault(sourceJsonObject: JsonObject, propertyName: String) {
     val value = (sourceJsonObject.get(propertyName) as? JsonArray) ?: JsonArray()
     add(propertyName, value)
+  }
+
+  internal sealed class AttributeProperty(
+      val name: String
+  ) {
+
+    class StringType(name: String) : AttributeProperty(name)
+
+    class ArrayType(name: String) : AttributeProperty(name)
   }
 }
